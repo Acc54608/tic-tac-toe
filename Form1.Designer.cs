994308d0fb5d451f9bcc2c974cd32e641eaeb3f9
Alexander Casper <acasper71@sccsc.edu>
2021-08-20 13:30:15 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tile1 = new System.Windows.Forms.Label();
+            this.tile3 = new System.Windows.Forms.Label();
+            this.tile2 = new System.Windows.Forms.Label();
+            this.tile4 = new System.Windows.Forms.Label();
+            this.tile5 = new System.Windows.Forms.Label();
+            this.tile6 = new System.Windows.Forms.Label();
+            this.tile7 = new System.Windows.Forms.Label();
+            this.tile9 = new System.Windows.Forms.Label();
+            this.tile8 = new System.Windows.Forms.Label();
+            this.newGame = new System.Windows.Forms.Button();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.gbTile = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbTile.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tile1
+            // 
+            this.tile1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tile1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tile1.Location = new System.Drawing.Point(57, 16);
+            this.tile1.Name = "tile1";
+            this.tile1.Size = new System.Drawing.Size(100, 100);
+            this.tile1.TabIndex = 0;
+            this.tile1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tile3
+            // 
+            this.tile3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tile3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tile3.Location = new System.Drawing.Point(561, 16);
+            this.tile3.Name = "tile3";
+            this.tile3.Size = new System.Drawing.Size(100, 100);
+            this.tile3.TabIndex = 2;
+            this.tile3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tile2
+            // 
+            this.tile2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tile2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tile2.Location = new System.Drawing.Point(302, 16);
+            this.tile2.Name = "tile2";
+            this.tile2.Size = new System.Drawing.Size(100, 100);
+            this.tile2.TabIndex = 1;
+            this.tile2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tile4
+            // 
+            this.tile4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tile4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tile4.Location = new System.Drawing.Point(57, 194);
+            this.tile4.Name = "tile4";
+            this.tile4.Size = new System.Drawing.Size(100, 100);
+            this.tile4.TabIndex = 3;
+            this.tile4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tile5
+            // 
+            this.tile5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tile5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tile5.Location = new System.Drawing.Point(302, 194);
+            this.tile5.Name = "tile5";
+            this.tile5.Size = new System.Drawing.Size(100, 100);
+            this.tile5.TabIndex = 4;
+            this.tile5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tile6
+            // 
+            this.tile6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tile6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tile6.Location = new System.Drawing.Point(561, 194);
+            this.tile6.Name = "tile6";
+            this.tile6.Size = new System.Drawing.Size(100, 100);
+            this.tile6.TabIndex = 5;
+            this.tile6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tile7
+            // 
+            this.tile7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tile7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tile7.Location = new System.Drawing.Point(57, 365);
+            this.tile7.Name = "tile7";
+            this.tile7.Size = new System.Drawing.Size(100, 100);
+            this.tile7.TabIndex = 6;
+            this.tile7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tile9
+            // 
+            this.tile9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tile9.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tile9.Location = new System.Drawing.Point(561, 365);
+            this.tile9.Name = "tile9";
+            this.tile9.Size = new System.Drawing.Size(100, 100);
+            this.tile9.TabIndex = 8;
+            this.tile9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tile8
+            // 
+            this.tile8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tile8.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tile8.Location = new System.Drawing.Point(302, 365);
+            this.tile8.Name = "tile8";
+            this.tile8.Size = new System.Drawing.Size(100, 100);
+            this.tile8.TabIndex = 7;
+            this.tile8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // newGame
+            // 
+            this.newGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newGame.Location = new System.Drawing.Point(171, 501);
+            this.newGame.Name = "newGame";
+            this.newGame.Size = new System.Drawing.Size(112, 33);
+            this.newGame.TabIndex = 9;
+            this.newGame.Text = "New Game";
+            this.newGame.UseVisualStyleBackColor = true;
+            this.newGame.Click += new System.EventHandler(this.newGame_Click);
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblWinner.Location = new System.Drawing.Point(316, 498);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(139, 36);
+            this.lblWinner.TabIndex = 10;
+            this.lblWinner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(501, 501);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 33);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // gbTile
+            // 
+            this.gbTile.BackColor = System.Drawing.Color.Transparent;
+            this.gbTile.Controls.Add(this.tile1);
+            this.gbTile.Controls.Add(this.tile2);
+            this.gbTile.Controls.Add(this.tile3);
+            this.gbTile.Controls.Add(this.tile9);
+            this.gbTile.Controls.Add(this.tile8);
+            this.gbTile.Controls.Add(this.tile6);
+            this.gbTile.Controls.Add(this.tile5);
+            this.gbTile.Controls.Add(this.tile7);
+            this.gbTile.Controls.Add(this.tile4);
+            this.gbTile.Location = new System.Drawing.Point(32, 4);
+            this.gbTile.Name = "gbTile";
+            this.gbTile.Size = new System.Drawing.Size(698, 477);
+            this.gbTile.TabIndex = 12;
+            this.gbTile.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(360, 549);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 91);
+            this.label1.TabIndex = 13;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 649);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.newGame);
+            this.Controls.Add(this.gbTile);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.gbTile.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tile1;
+        private System.Windows.Forms.Label tile3;
+        private System.Windows.Forms.Label tile2;
+        private System.Windows.Forms.Label tile4;
+        private System.Windows.Forms.Label tile5;
+        private System.Windows.Forms.Label tile6;
+        private System.Windows.Forms.Label tile7;
+        private System.Windows.Forms.Label tile9;
+        private System.Windows.Forms.Label tile8;
+        private System.Windows.Forms.Button newGame;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox gbTile;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+        int[,] ticTacToe = new int[3, 3];
+        Random rnd1 = new Random();
+        Boolean xWin;
+        Boolean oWin;
+        String arrStr = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newGame_Click(object sender, EventArgs e)
+        {
+            arrStr = "";
+            xWin = false;
+            oWin = false;
+            lblWinner.Text = "";
+            List<Label> tiles = new List<Label>{ tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9 };
+            int tile = 0;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    ticTacToe[i, j] = rnd1.Next(2);
+                    if (ticTacToe[i, j] == 0)
+                        tiles[tile].Text = "O";
+                    else
+                        tiles[tile].Text = "X";
+                    tile++;
+                }
+            WhoWon(ticTacToe);
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    arrStr += ticTacToe[i, j].ToString();
+            label1.Text = arrStr;
+        }
+
+        private void WhoWon(int[,] ticTacToe)
+        {
+            xWin = false;
+            oWin = false;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (ticTacToe[i, 0] == 0 && ticTacToe[i, 1] == 0 && ticTacToe[i, 2] == 0)
+                    oWin = true;
+                if (ticTacToe[i, 0] == 1 && ticTacToe[i, 1] == 1 && ticTacToe[i, 2] == 1)
+                    xWin = true;
+
+            }
+
+            for (int j = 0; j < 3; j++)
+            {
+                if (ticTacToe[0, j] == 0 && ticTacToe[1, j] == 0 && ticTacToe[2, j] == 0)
+                    oWin = true;
+                if (ticTacToe[0, j] == 1 && ticTacToe[1, j] == 1 && ticTacToe[2, j] == 1)
+                    xWin = true;
+            }
+
+            if (ticTacToe[0, 0] == 0 && ticTacToe[1, 1] == 0 && ticTacToe[2, 2] == 0)
+                oWin = true;
+            if (ticTacToe[0, 0] == 1 && ticTacToe[1, 1] == 1 && ticTacToe[2, 2] == 1)
+                xWin = true;
+
+            if (ticTacToe[0, 2] == 0 && ticTacToe[1, 1] == 0 && ticTacToe[2, 0] == 0)
+                oWin = true;
+            if (ticTacToe[0, 2] == 1 && ticTacToe[1, 1] == 1 && ticTacToe[2, 0] == 1)
+                xWin = true;
+
+            if (xWin && oWin)
+                lblWinner.Text = "It's a Tie";
+            if (!xWin && !oWin)
+                lblWinner.Text = "Both lost :(";
+            if (xWin && !oWin)
+                lblWinner.Text = "X Wins!";
+            if (oWin && !xWin)
+                lblWinner.Text = "O Wins!";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
